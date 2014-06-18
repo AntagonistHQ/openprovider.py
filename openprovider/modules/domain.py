31e5eda308d412ac9a0a75580961fb666cbdfdb4
Wander Nauta <info@wandernauta.nl>
2014-06-18 23:32:39 +0200

    Abstract XML generation for check cmd into own method

--- openprovider/modules/domain.py
+++ openprovider/modules/domain.py
@@ -9,15 +9,18 @@ class DomainModule(common.Module):
         Check availability for a single domain. Returns the domain's status
         as a string (either "active" or "free").
         """
-        e = self.e
-        (name, extension) = domain.split(".")
-
-        response = self.request(
-            e.checkDomainRequest(
-                e.domains(
-                    e.array(
-                        e.item(
-                            e.name(name),
-                            e.extension(extension))))))
-
+        response = self.request(self._check_cmd([domain]))
         return response.data.array[0].item[0].status
+
+    def _check_cmd(self, domains):
+        e = self.e
+        return e.checkDomainRequest(
+            e.domains(
+                e.array(
+                    *[e.item(
+                        e.name(domain.split(".")[0]),
+                        e.extension(domain.split(".")[1])
+                    ) for domain in domains]
+                )
+            )
+        )
