0d76d0442ad4785eaec07e7a35b341d23ba15713
Wander Nauta <info@wandernauta.nl>
2014-07-15 10:23:16 +0200

    Add cert types and utility methods

--- openprovider/data/sslcerts.py
+++ openprovider/data/sslcerts.py
@@ -43,3 +43,67 @@ class CertType(object):
         COMODO_PREMIUMSSL_WILDCARD,
         COMODO_INSTANT_SGC_WILDCARD_SSL,
         RAPIDSSL_WILDCARD)
+
+    DV_CERTIFICATES = (
+        GEOTRUST_QUICKSSL_PREMIUM_DV,
+        THAWTE_SSL_123,
+        COMODO_ESSENTIALSSL,
+        RAPIDSSL)
+
+    OV_CERTIFICATES = (
+        VERISIGN_SS,
+        GEOTRUST_TBID,
+        THAWTE_WEB_SERVER,
+        COMODO_PREMIUMSSL,
+        COMODO_INSTANTSSL_PRO,
+        COMODO_INSTANTSSL)
+
+    EV_CERTIFICATES = (
+        VERISIGN_SS_EV,
+        GEOTRUST_TBID_EV,
+        THAWTE_WEB_SERVER_EV,
+        COMODO_EV_SSL)
+
+    SGC_CERTIFICATES = (
+        VERISIGN_SS_PRO_EV,
+        VERISIGN_SS_PRO,
+        THAWTE_SGC_SUPERCERT,
+        COMODO_INSTANT_SGC_WILDCARD_SSL,
+        COMODO_EV_SGC_SSL_EV,
+        COMODO_INSTANT_SGC_SSL)
+
+    MULTI_CERTIFICATES = (
+        GEOTRUST_TBID_EV_MULTI_DOMAIN,
+        GEOTRUST_TBID_MULTI_DOMAIN,
+        COMODO_EVSSL_MULTI_DOMAIN,
+        COMODO_UCC)
+
+    @staticmethod
+    def is_wildcard(cert):
+        """Returns True if this is a wildcard certificate."""
+        return cert in CertType.WILDCARD_CERTIFICATES
+
+    @staticmethod
+    def is_dv(cert):
+        """Returns True if this is a domain-validated certificate."""
+        return cert in CertType.DV_CERTIFICATES
+
+    @staticmethod
+    def is_ov(cert):
+        """Returns True if this is an organization-validated certificate."""
+        return cert in CertType.OV_CERTIFICATES
+
+    @staticmethod
+    def is_ev(cert):
+        """Returns True if this is an Extended validation cert."""
+        return cert in CertType.EV_CERTIFICATES
+
+    @staticmethod
+    def is_sgc(cert):
+        """Returns True if this is a server-gated cryptography certificate."""
+        return cert in CertType.SGC_CERTIFICATES
+
+    @staticmethod
+    def is_multidomain(cert):
+        """Returns True if this is a multi-domain certificate."""
+        return cert in CertType.MULTI_CERTIFICATES
