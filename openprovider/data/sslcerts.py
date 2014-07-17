a2fdca01580528a5cd87b4ae4e466472bca3df67
Wander Nauta <wnauta@antagonist.nl>
2014-07-17 17:35:00 +0200

    Make certificate types CertTypes

--- openprovider/data/sslcerts.py
+++ openprovider/data/sslcerts.py
@@ -4,106 +4,65 @@
 
 
 class CertType(object):
-    """Constants that can be used as SSL certificate product ID's."""
+    """A certificate type that can be purchased from OpenProvider."""
 
-    VERISIGN_SS = 1
-    VERISIGN_SS_PRO = 2
-    VERISIGN_SS_EV = 3
-    VERISIGN_SS_PRO_EV = 4
-    GEOTRUST_QUICKSSL_PREMIUM_DV = 8
-    GEOTRUST_TBID = 9
-    GEOTRUST_TBID_EV = 10
-    GEOTRUST_TBID_MULTI_DOMAIN = 34
-    GEOTRUST_TBID_EV_MULTI_DOMAIN = 30
-    GEOTRUST_TBID_WILDCARD = 11
-    THAWTE_SSL_123 = 14
-    THAWTE_WEB_SERVER = 15
-    THAWTE_WEB_SERVER_EV = 16
-    THAWTE_WEB_SERVER_WILDCARD = 17
-    THAWTE_SGC_SUPERCERT = 18
-    COMODO_ESSENTIALSSL = 31
-    COMODO_INSTANTSSL = 20
-    COMODO_INSTANTSSL_PRO = 21
-    COMODO_PREMIUMSSL = 22
-    COMODO_EV_SSL = 24
-    COMODO_EV_SGC_SSL_EV = 27
-    COMODO_INSTANT_SGC_SSL = 25
-    COMODO_UCC = 28
-    COMODO_EVSSL_MULTI_DOMAIN = 33
-    COMODO_ESSENTIALSSL_WILDCARD = 32
-    COMODO_PREMIUMSSL_WILDCARD = 23
-    COMODO_INSTANT_SGC_WILDCARD_SSL = 26
-    RAPIDSSL = 5
-    RAPIDSSL_WILDCARD = 6
+    product_id = 0
+    vendor = ""
+    name = ""
+    validation = None
+    is_multi = False
+    is_sgc = False
+    is_wildcard = False
 
-    WILDCARD_CERTIFICATES = (
-        GEOTRUST_TBID_WILDCARD,
-        THAWTE_WEB_SERVER_WILDCARD,
-        COMODO_ESSENTIALSSL_WILDCARD,
-        COMODO_PREMIUMSSL_WILDCARD,
-        COMODO_INSTANT_SGC_WILDCARD_SSL,
-        RAPIDSSL_WILDCARD)
+    def __init__(self, product_id, vendor, name, validation, **kwargs):
+        self.product_id = product_id
+        self.vendor = vendor
+        self.name = name
+        self.validation = validation
+        self.__dict__.update(kwargs)
 
-    DV_CERTIFICATES = (
-        GEOTRUST_QUICKSSL_PREMIUM_DV,
-        THAWTE_SSL_123,
-        COMODO_ESSENTIALSSL,
-        RAPIDSSL)
 
-    OV_CERTIFICATES = (
-        VERISIGN_SS,
-        GEOTRUST_TBID,
-        THAWTE_WEB_SERVER,
-        COMODO_PREMIUMSSL,
-        COMODO_INSTANTSSL_PRO,
-        COMODO_INSTANTSSL)
+class CertTypes(object):
+    """
+    A class that contains constants that can be used as SSL certificate product
+    ID's.
+    """
 
-    EV_CERTIFICATES = (
-        VERISIGN_SS_EV,
-        GEOTRUST_TBID_EV,
-        THAWTE_WEB_SERVER_EV,
-        COMODO_EV_SSL)
+    VERISIGN_SS = CertType(1, "Verisign", "Secure Site", "OV")
+    VERISIGN_SS = CertType(1, "", "", "")
+    VERISIGN_SS_PRO = CertType(2, "", "", "")
+    VERISIGN_SS_EV = CertType(3, "", "", "")
+    VERISIGN_SS_PRO_EV = CertType(4, "", "", "")
+    GEOTRUST_QUICKSSL_PREMIUM_DV = CertType(8, "", "", "")
+    GEOTRUST_TBID = CertType(9, "", "", "")
+    GEOTRUST_TBID_EV = CertType(10, "", "", "")
+    GEOTRUST_TBID_MULTI_DOMAIN = CertType(34, "", "", "")
+    GEOTRUST_TBID_EV_MULTI_DOMAIN = CertType(30, "", "", "")
+    GEOTRUST_TBID_WILDCARD = CertType(11, "", "", "")
+    THAWTE_SSL_123 = CertType(14, "", "", "")
+    THAWTE_WEB_SERVER = CertType(15, "", "", "")
+    THAWTE_WEB_SERVER_EV = CertType(16, "", "", "")
+    THAWTE_WEB_SERVER_WILDCARD = CertType(17, "", "", "")
+    THAWTE_SGC_SUPERCERT = CertType(18, "", "", "")
+    COMODO_ESSENTIALSSL = CertType(31, "", "", "")
+    COMODO_INSTANTSSL = CertType(20, "", "", "")
+    COMODO_INSTANTSSL_PRO = CertType(21, "", "", "")
+    COMODO_PREMIUMSSL = CertType(22, "", "", "")
+    COMODO_EV_SSL = CertType(24, "", "", "")
+    COMODO_EV_SGC_SSL_EV = CertType(27, "", "", "")
+    COMODO_INSTANT_SGC_SSL = CertType(25, "", "", "")
+    COMODO_UCC = CertType(28, "", "", "")
+    COMODO_EVSSL_MULTI_DOMAIN = CertType(33, "", "", "")
+    COMODO_ESSENTIALSSL_WILDCARD = CertType(32, "", "", "")
+    COMODO_PREMIUMSSL_WILDCARD = CertType(23, "", "", "")
+    COMODO_INSTANT_SGC_WILDCARD_SSL = CertType(26, "", "", "")
+    RAPIDSSL = CertType(5, "", "", "")
+    RAPIDSSL_WILDCARD = CertType(6, "", "", "")
 
-    SGC_CERTIFICATES = (
-        VERISIGN_SS_PRO_EV,
-        VERISIGN_SS_PRO,
-        THAWTE_SGC_SUPERCERT,
-        COMODO_INSTANT_SGC_WILDCARD_SSL,
-        COMODO_EV_SGC_SSL_EV,
-        COMODO_INSTANT_SGC_SSL)
+    @classmethod
+    def all(cls):
+        return filter(lambda x: isinstance(x, CertType), cls.__dict__.values())
 
-    MULTI_CERTIFICATES = (
-        GEOTRUST_TBID_EV_MULTI_DOMAIN,
-        GEOTRUST_TBID_MULTI_DOMAIN,
-        COMODO_EVSSL_MULTI_DOMAIN,
-        COMODO_UCC)
-
-    @staticmethod
-    def is_wildcard(cert):
-        """Returns True if this is a wildcard certificate."""
-        return cert in CertType.WILDCARD_CERTIFICATES
-
-    @staticmethod
-    def is_dv(cert):
-        """Returns True if this is a domain-validated certificate."""
-        return cert in CertType.DV_CERTIFICATES
-
-    @staticmethod
-    def is_ov(cert):
-        """Returns True if this is an organization-validated certificate."""
-        return cert in CertType.OV_CERTIFICATES
-
-    @staticmethod
-    def is_ev(cert):
-        """Returns True if this is an Extended validation cert."""
-        return cert in CertType.EV_CERTIFICATES
-
-    @staticmethod
-    def is_sgc(cert):
-        """Returns True if this is a server-gated cryptography certificate."""
-        return cert in CertType.SGC_CERTIFICATES
-
-    @staticmethod
-    def is_multidomain(cert):
-        """Returns True if this is a multi-domain certificate."""
-        return cert in CertType.MULTI_CERTIFICATES
+    @classmethod
+    def dv_certs(cls):
+        return filter(lambda x: x.is_dv, cls.all())
