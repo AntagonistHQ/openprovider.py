9143f844a91f9d8eb76645fd284eeb75ffff61aa
Ewoud Kohl van Wijngaarden <ekohlvanwijngaarden@antagonist.nl>
2015-01-16 12:20:48 +0100

    Version 0.8.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='openprovider.py',
-    version='0.7.0',
+    version='0.8.0',
     author='Antagonist B.V.',
     author_email='info@antagonist.nl',
     packages=['openprovider', 'openprovider.modules', 'openprovider.data'],
