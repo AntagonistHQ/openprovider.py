fdd8b7dd200de058adeda8c8638b361af0a2639f
Tiemo Kieft <tiemo@macbook.local>
2020-10-05 10:19:36 +0200

    Version 0.11.3

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ else:
 
 setup(
     name='openprovider.py',
-    version='0.11.1',
+    version='0.11.3',
     author='Antagonist B.V.',
     author_email='info@antagonist.nl',
     packages=['openprovider', 'openprovider.modules', 'openprovider.data'],
